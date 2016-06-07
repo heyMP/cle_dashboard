1be3a7ecd95243182c6915e52acb02a8641a8fae
heyMP <mgp140@gmail.com>
2016-06-07 16:02:28 -0400

    Added tags and active classes

--- templates/cle-dashboard.tpl.php
+++ templates/cle-dashboard.tpl.php
@@ -5,7 +5,13 @@
     <a href="{{basePath}}node/{{assignment.nid}}" ng-repeat="assignment in data.assignments" ng-cloak>
       <div class="collapsible-header"><i class="material-icons">assignment</i> 
         <b>{{assignment.node_title}}</b>
-        <span class="secondary-content" ng-if="assignment.field_field_due_date"> Due Date: {{assignment.field_field_due_date[0].raw.value}} </span>
+        <div class="secondary-content">
+          <span class="chip" ng-if="assignment.active"> Active </span>
+        </div>
+        <div class="post-date">
+          <span> Due: </span>
+          <span ng-bind-html="assignment.field_field_begin_date[0].rendered['#markup']"></span> &ndash; <span ng-bind-html="assignment.field_field_due_date[0].rendered['#markup']"></span>
+        </div>
       </div>
     </a>
   </div>
@@ -15,7 +21,13 @@
     <a href="{{basePath}}node/{{submission.nid}}" class="avatar" ng-repeat="submission in data.submissions" ng-cloak>
       <div class="collapsible-header"><i class="material-icons">subject</i> 
         <b>{{submission.node_title}}</b> 
-        <span class="secondary-content" ng-if="submission.node_created"> Posted: {{submission.node_created}}</span>
+        <div class="secondary-content">
+          <span class="chip" ng-bind-html="submission.field_field_submission_state[0]['rendered']['#markup']"></span>
+        </div>
+
+        <div class="post-date" ng-if="submission.node_created">
+          <span> Posted: </span> {{ submission.node_created | date: 'EEE, MM-dd-yy'}}
+        </div>
       </div>
     </a>
   </div>
@@ -23,9 +35,11 @@
   <div class="collection with-header">
     <div class="collection-header"><h3>My Critiques</h3></div>
     <a href="{{basePath}}node/{{critique.nid}}" class="avatar" ng-repeat="critique in data.critiques" ng-cloak>
-      <div class="collapsible-header"><i class="material-icons">assignment</i> 
-        <b>{{submission.node_title}}</b> 
-        <span class="secondary-content" ng-if="critique.node_created"> Posted: {{critique.node_created}}</span>
+      <div class="collapsible-header"><i class="material-icons"> comment </i> 
+        <b>{{critique.node_title}}</b> 
+        <div class="post-date" ng-if="critique.node_created">
+          <span> Posted: </span> {{ critique.node_created | date: 'EEE, MM-dd-yy'}}
+        </div>
       </div>
     </a>
   </div>
